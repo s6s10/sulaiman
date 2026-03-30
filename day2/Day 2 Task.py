153aebae2c7845c32c947965fcfc07660baa9ece
Sulaiman <sulieman3600@gmail.com>
2026-03-30 15:03:53 +0400

    update ip validator

--- day2/Day 2 Task.py
+++ day2/Day 2 Task.py
@@ -1,78 +1,78 @@
-# This function checks if the IP address is valid
+# function checks if ipaddress?
 def check_ip(ip):
-    # Split the IP address using dots
+    # split ip by dots same day 1
     parts = ip.split(".")
 
-    # IP address must have 4 parts
+    # ip have 4 parts
     if len(parts) != 4:
-        raise ValueError("Invalid IP address")
+        raise ValueError("invalid ipaddress")
 
-    # Check each part of the IP
+    # Check ip parts
     for part in parts:
-        # Change the part from string to integer
+        # change from str to int
         num = int(part)
 
-        # Each number in IP must be between 0 and 255
+        # ip between 0 and 255
         if num < 0 or num > 255:
-            raise ValueError("Invalid IP address")
+            raise ValueError("invalid ipaddress")
 
 
-# This function checks if the CIDR is valid
+# checks if CIDR valid
 def check_cidr(cidr):
-    # CIDR must be from 0 to 32
+    # CIDR from 0 to 32
     if cidr < 0 or cidr > 32:
-        raise ValueError("Invalid CIDR prefix")
+        raise ValueError("invalid CIDR prefix")
 
 
-# This function changes the IP address to binary
+# changes ipaddress to binary
 def ip_to_binary(ip):
-    # Split the IP into 4 parts
+    # split 4 parts
     parts = ip.split(".")
     binary_ip = ""
 
-    # Change each part to 8-bit binary and add it to one string
+    # change to 8-bit binary && add in one_string
     for part in parts:
         binary_ip += format(int(part), "08b")
 
     return binary_ip
 
 
-# This function changes binary back to normal IP address
+# changes binary back to ipaddress
 def binary_to_ip(binary_ip):
     ip_parts = []
 
-    # Take the binary string 8 bits at a time
+    # binary string 8 bits
     for i in range(0, 32, 8):
         part = binary_ip[i:i+8]
 
-        # Change each 8-bit part to decimal and save it
+        # 8bit part to decimal
         ip_parts.append(str(int(part, 2)))
 
-    # Join the 4 parts with dots
+    # join the 4 parts && .
     return ".".join(ip_parts)
 
 
-# This function finds the network address
+# finds the network address
 def get_network_address(binary_ip, cidr):
-    # Keep the network bits and make the rest 0
+    # make the rest 0
     network_binary = binary_ip[:cidr] + "0" * (32 - cidr)
 
-    # Change binary result to IP address
+    # binary to IP address
     return binary_to_ip(network_binary)
 
 
-# This function finds the broadcast address
+# finds the broadcast address
 def get_broadcast_address(binary_ip, cidr):
-    # Keep the network bits and make the rest 1
+    # make the rest 1
     broadcast_binary = binary_ip[:cidr] + "1" * (32 - cidr)
 
-    # Change binary result to IP address
+    # binary to IP address
     return binary_to_ip(broadcast_binary)
 
 
-# This function calculates the number of usable hosts
+# calculates the number of hosts
 def get_usable_hosts(cidr):
-    # /31 and /32 do not have usable hosts in this program
+    # /31 and /32 do not have usable hosts
     if cidr >= 31:
         return 0
 
@@ -80,40 +80,37 @@ def get_usable_hosts(cidr):
     return (2 ** (32 - cidr)) - 2
 
 
-# Print the title
+# print the title
 print("--- Subnet Calculator ---")
 
 try:
-    # Ask the user to enter IP address
+    # enter IP address
     ip = input("Enter an IP address: ")
 
-    # Ask the user to enter CIDR and change it to integer
+    # enter CIDR 
     cidr = int(input("Enter CIDR prefix: "))
 
-    # Check if IP and CIDR are valid
+    # ip and CIDR are ok ??
     check_ip(ip)
     check_cidr(cidr)
 
-    # Change the IP address to binary
+    # ipaddress to binary
     binary_ip = ip_to_binary(ip)
 
-    # Get the network address
+    # network address
     network_address = get_network_address(binary_ip, cidr)
 
-    # Get the broadcast address
+    # broadcast address
     broadcast_address = get_broadcast_address(binary_ip, cidr)
 
-    # Get the number of usable hosts
+    # Gusable hosts
     usable_hosts = get_usable_hosts(cidr)
 
-    # Print the results
-    print("Network Address:", network_address)
-    print("Broadcast Address:", broadcast_address)
-    print("Number of Usable Hosts:", usable_hosts)
+    # results
+    print("network address:", network_address)
+    print("broadcast address:", broadcast_address)
+    print("number of usable hosts:", usable_hosts)
 
-# If there is any error, print the error message
+# any error??, error message
 except Exception as e:
     print("Error: Invalid IP address or CIDR prefix provided. Details:", e)
-
-# Print the ending line
-print("-------------------------")
